bb8849e5ad86c5b8492a87587db83ea6848845a3
donghyeon95 <smdf2545@konkuck.ac.kr>
2020-08-17 16:56:03 +0900

    modify TEMPLATS_DIRS in settings.py

--- kuding/kuding/settings.py
+++ kuding/kuding/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'kuding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'kuding/templates'],
+        'DIRS': [BASE_DIR, 'myenv/kuding/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
