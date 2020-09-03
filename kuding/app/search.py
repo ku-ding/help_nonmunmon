adaf43986c6f6f6a5da8cdd913dbe673f3868498
harheem <shhr.kre@gmail.com>
2020-09-03 22:28:03 +0900

    Modify functions to improve similarity accuracy

--- kuding/app/search.py
+++ kuding/app/search.py
@@ -1,11 +1,12 @@
 from elasticsearch import Elasticsearch
 from datetime import datetime
 
+index_name = "dictionary-*"
+
 #User can input custum dictionary
 def insert_data(title, content, url, image, category, who):
     es = Elasticsearch()
-
-    doc = {   
+    doc = {
         "title": title,
         "contenct": content,
         "imgae": image,
@@ -20,49 +21,57 @@ def insert_data(title, content, url, image, category, who):
 
 def search_data_content(title, content):
     es = Elasticsearch()
-    body = {           
-        "query": {
-		    "bool": {
-		        "should": [{ "match": { "content" : content} }], 
-                "must": [{ "match": {"title": title} }]
+    body = 	{
+    "query": {
+		"bool": {
+		  "should" : [ {
+          "multi_match" : {
+            "boost": 5, 
+            "query": content,
+            "type": "cross_fields",
+            "fields": [ "content", "content.korean", "content.english" ] } } ]
+      , "must": [
+        {
+          "multi_match" : {
+            "query": title,
+            "type": "cross_fields",
+            "fields": [ "title", "title.korean", "title.english" ] } } ]
 		    }
-	    },
-	    "_source": {
-		    "includes": ["title", "content", "category", "image", "url", "who"]
-	    }
+	    }, "_source": { "includes": ["title", "content", "category", "image", "url", "who"] }
     }
 
-    return es.search(index="dictionary-*", body=body)
+    return es.search(index= index_name, body=body)
 
 def search_data_category(title, category):
     es = Elasticsearch()
-    body = {           
-        "query": {
-		    "bool": {
-		        "filter": [{ "match": { "category" : category} }], 
-                "must": [{ "match": {"title": title} }]
-		    }
-	    },
-	    "_source": {
-		    "includes": ["title", "content", "category", "image", "url", "who"]
-	    }
+    body = {
+    "query": {
+        "bool": {
+            "filter": [{ "match": { "category" : category} } ],
+            "must": [ {
+            "multi_match" : {
+            "query": title,
+            "type": "cross_fields",
+            "fields": [ "title", "title.korean", "title.english" ] } } ]     
+            }
+        }, "_source": { "includes": ["title", "content", "category", "image", "url", "who"] }
     }
 
-    return es.search(index="dictionary-*", body=body)
+    return es.search(index=index_name, body=body)
 
 def search_data_who(title, who):
     es = Elasticsearch()
-    body = {           
-        "query": {
-		    "bool": {
-		        "filter": [{ "match": { "who" : who} }], 
-                "must": [{ "match": {"title": title} }]
-		    }
-	    },
-	    "_source": {
-		    "includes": ["title", "content", "category", "image", "url", "who"]
-	    }
+    body = {
+    "query": {
+        "bool": {
+            "filter": [{ "match": { "who" : who} } ],
+            "must": [ {
+            "multi_match" : {
+            "query": title,
+            "type": "cross_fields",
+            "fields": [ "title", "title.korean", "title.english" ] } } ]
+            }
+        }, "_source": { "includes": ["title", "content", "category", "image", "url", "who"] }
     }
 
-    return es.search(index="dictionary-*", body=body)
-
+    return es.search(index=index_name, body=body)
